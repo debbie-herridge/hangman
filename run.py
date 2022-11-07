faefc42f97c5778d6588932d2d98cbc4eb654345
debbie-herridge <debbie.herridge@icloud.com>
2022-11-07 16:36:40 +0000

    inital commit and game intro

--- run.py
+++ run.py
@@ -1,3 +1,8 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+print('Welcome to the Hangman game!')
+print('Rules: you have 5 chances to guess the letters in the word, each time you guess \nwrong you lose a limb..on the last try its game over. Good luck and stay alive!\n')
+
+user_name = input('Before we begin, can I take your name? \n')
